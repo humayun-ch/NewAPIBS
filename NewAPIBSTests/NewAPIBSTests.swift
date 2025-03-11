3cbfb455c18e999b479d0d2ea1bb8c816af39c0f
humayun-ch <kabirchanchal07@gmail.com>
2025-03-11 13:32:54 +0600

    Initial Commit

---
+++ NewAPIBSTests/NewAPIBSTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewAPIBSTests.swift
+//  NewAPIBSTests
+//
+//  Created by Humayun Kabir on 11/3/25.
+//
+
+import Testing
+@testable import NewAPIBS
+
+struct NewAPIBSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

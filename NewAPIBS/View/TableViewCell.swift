2d7f4d135b65f45403c157c53265e15e32dcaa31
humayun-ch <kabirchanchal07@gmail.com>
2025-03-11 14:26:14 +0600

    Tableview cell designed

---
+++ NewAPIBS/View/TableViewCell.swift
@@ -0,0 +1,129 @@
+//
+//  TableViewCell.swift
+//  NewAPIBS
+//
+//  Created by Humayun Kabir on 11/3/25.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    // MARK: - UI Elements
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .darkGray
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    private let newsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+    
+    private let urlToImageLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = .blue
+        label.numberOfLines = 2
+        label.lineBreakMode = .byTruncatingMiddle
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .yellow
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - TableView Cell Setup
+    private func setupLayout() {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel, newsImageView, urlToImageLabel, dateLabel, contentLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(stackView)
+        
+        newsImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            
+            newsImageView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            newsImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    // MARK: - Configure Cell
+    func configure(with article: Article) {
+        titleLabel.text = article.title
+        descriptionLabel.text = article.description
+        contentLabel.text = article.content
+        dateLabel.text = formatDate(article.publishedAt)
+        urlToImageLabel.text = article.urlToImage ?? "No Image URL"
+        loadImage(from: article.urlToImage)
+    }
+    
+    private func loadImage(from urlString: String?) {
+        guard let urlString = urlString, let url = URL(string: urlString) else {
+            newsImageView.image = UIImage(named: "placeholderImage")
+            return
+        }
+        
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.newsImageView.image = image
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.newsImageView.image = UIImage(named: "placeholderImage")
+                }
+            }
+        }
+    }
+    
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
+    }
+}

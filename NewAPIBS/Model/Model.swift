5d73426824de130afa646bc5b4c1de83b650ad9d
humayun-ch <kabirchanchal07@gmail.com>
2025-03-11 13:46:25 +0600

    👍 Model Updated

--- NewAPIBS/Model/Model.swift
+++ NewAPIBS/Model/Model.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let author: String?
+    let title, description: String
+    let url: String
+    let urlToImage: String?
+    let publishedAt: Date
+    let content: String
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: ID?
+    let name: String
+}
+
+enum ID: String, Codable {
+    case espn = "espn"
+    case theVerge = "the-verge"
+    case wired = "wired"
+}
+
+
